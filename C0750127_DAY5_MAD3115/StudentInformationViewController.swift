6beac2c99c5c982dfb3ee09e009224c000e82901
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2019-03-12 18:35:42 -0400

    need to revise

---
+++ C0750127_DAY5_MAD3115/StudentInformationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StudentInformationViewController.swift
+//  C0750127_DAY5_MAD3115
+//
+//  Created by Baturay Kayatürk on 2019-03-11.
+//  Copyright © 2019 Lambton College. All rights reserved.
+//
+
+import UIKit
+
+class StudentInformationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ C0750127_DAY5_MAD3115/StudentsTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  StudentsTableViewCell.swift
+//  C0750127_DAY5_MAD3115
+//
+//  Created by Baturay Kayatürk on 2019-03-11.
+//  Copyright © 2019 Lambton College. All rights reserved.
+//
+
+import UIKit
+
+class StudentsTableViewCell: UITableViewCell {
+
+   
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
